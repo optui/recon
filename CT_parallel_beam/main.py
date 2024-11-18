aa7147afd4a2b2493f2845bfd0642e2c878d7d3d
optui <h149342@stud.u-szeged.hu>
2024-11-18 14:32:20 +0100

    Cleaning

--- CT_parallel_beam/main.py
+++ CT_parallel_beam/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import opengate as gate
 from scipy.spatial.transform import Rotation
 
-# Units for convenience
+# Units
 m = gate.g4_units.m
 cm = gate.g4_units.cm
 mm = gate.g4_units.mm
@@ -14,25 +14,27 @@ Bq = gate.g4_units.Bq
 if __name__ == "__main__":
     sim = gate.Simulation()
 
+    # Simulation parameters
     sim.visu = False
     sim.verbose_level = 'DEBUG'
 
-    # Define world
-    sim.world.size = [2 * m] * 3
+    # World
+    world = sim.world
+    world.size = [2 * m] * 3
 
-    # Add a water volume
+    # Volume
     vol = sim.add_volume("Box", "vol")
-    vol.size = [10 * cm, 10 * cm, 10 * cm]
+    vol.size = [10 * cm] * 3
     vol.material = "G4_WATER"
     vol.color = [0, 0.5, 1, 1]
 
-    # Add a detector volume
+    # Detector
     detector = sim.add_volume("Box", "detector")
     detector.material = "G4_Si"
     detector.size = [20 * cm, 5 * mm, 20 * cm]
     detector.translation = [0, -0.5 * m, 0]
 
-    # Add a gamma source
+    # Source
     source = sim.add_source("GenericSource", "parallel_source")
     source.particle = "gamma"
     source.activity = 1e5 * Bq
@@ -43,16 +45,17 @@ if __name__ == "__main__":
     source.direction.type = "momentum"
     source.direction.momentum = [0, -1, 0]
 
-
+    # Simulation Statistics
     stats_actor = sim.add_actor("SimulationStatisticsActor", "Stats")
     stats_actor.output_filename = "output/simulation_stats.txt"
 
-    # Add actors for digitization and projection
+    # Hits Collection
     hits_actor = sim.add_actor("DigitizerHitsCollectionActor", "Hits")
     hits_actor.attached_to = detector.name
     hits_actor.attributes = ['TotalEnergyDeposit', 'PostPosition', 'GlobalTime']
     hits_actor.output_filename = 'output/hits.root'
 
+    # Projection
     proj_actor = sim.add_actor("DigitizerProjectionActor", "Projection")
     proj_actor.attached_to = detector.name
     proj_actor.input_digi_collections = ["Hits"]
@@ -60,26 +63,22 @@ if __name__ == "__main__":
     proj_actor.size = [200, 200]
     proj_actor.origin_as_image_center = True
 
-    # Define rotations
-    angular_step = 5  # degrees
+    # Dynamic Rotation
+    angular_step = 5
     angles = np.arange(0, 180, angular_step)
 
-    # Precompute rotation matrices
+    # Compute rotation matrices
     rotations = [Rotation.from_euler('z', angle, degrees=True).as_matrix() for angle in angles]
 
-    # Set simulation timing intervals
+    # Simulation timing intervals parameter
     sim.run_timing_intervals = [[i * sec, (i + 1) * sec] for i in range(len(rotations))]
 
-    # Simulation loop to update rotation
     for idx, rotation in enumerate(rotations):
-        # Update the rotation of the volume
+        # Rotate the volume
         vol.rotation = rotation
-        print(f"Set rotation to {angles[idx]} degrees")
 
         proj_actor.output_filename = f'output/projections{idx}.mhd'
 
-        # Run the simulation for the current interval
+        # Run for current interval
         sim.run_timing_intervals = [[idx * sec, (idx + 1) * sec]]
         sim.run(start_new_process=True)
-
-    print("Simulation completed.")
